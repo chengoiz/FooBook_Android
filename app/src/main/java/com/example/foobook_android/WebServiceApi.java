17dd9db25c14c44b7789f0f00ce4a8d7efa1ecb3
chen <chengoizman@gmail.com>
2024-03-10 22:27:44 +0200

    Refactored SignUp classes to MVVM

--- app/src/main/java/com/example/foobook_android/WebServiceApi.java
+++ app/src/main/java/com/example/foobook_android/WebServiceApi.java
@@ -6,7 +6,16 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
-public interface WebServiceUserApi {
+public interface WebServiceApi {
+
+    // signUp
     @POST("api/users")
     Call<UserResponse> registerUser(@Body User user);
+/*
+    // login
+    @POST("api/tokens")
+    Call<LoginResponse> loginUser(@Body LoginRequest loginRequest);
+
+ */
 }
+
